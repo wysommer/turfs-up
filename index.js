3db5a6d4554cdaae7c0ae33048cc8588d349793c
wy <wyatt3996@gmail.com>
2024-12-17 10:02:58 -0700

    responsive

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.body.classList += " open-menu";
+    // document.body.classList.remove("close-menu");
+}
+
+function closeMenu() {
+    document.body.classList.remove("open-menu");
+    // document.body.classList += " close-menu";
+}
