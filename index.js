8a34046ca2938305468efbe2ab854a4c41245080
wy <wyatt3996@gmail.com>
2024-12-21 23:30:14 -0700

    almost done

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function openMenu() {
     document.body.classList += " open-menu";
-    // document.body.classList.remove("close-menu");
 }
 
 function closeMenu() {
     document.body.classList.remove("open-menu");
-    // document.body.classList += " close-menu";
 }
